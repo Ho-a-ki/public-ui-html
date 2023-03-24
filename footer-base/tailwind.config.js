22db0432fff965a3bcb31b07bfb8b55cbe590cf7
gh home desktop <gh4777@naver.com>
2023-03-24 23:54:35 +0900

    tailwind footer develop

---
+++ footer-base/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blue: "#2553a0",
+      white: "#ffffff",
+    },
+    fontFamily: {
+      "custom": ["Pretendard"],
+      sans: ['"Pretendard"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
