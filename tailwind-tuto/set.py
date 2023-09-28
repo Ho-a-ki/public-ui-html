609241b062513b1cc35ea81f5cc343456239c3d5
gh home desktop <gh4777@naver.com>
2023-09-28 14:05:22 +0900

    tailwind set.py add

---
+++ tailwind-tuto/set.py
@@ -0,0 +1,42 @@
+
+import os
+
+os.system("npx tailwindcss init")
+os.mkdir('src')
+
+with open('tailwind.config.js', 'w') as f:
+    f.write('''
+    module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    }            
+''')
+
+with open('src/input.css', 'w') as f:
+    f.write('''
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+''')
+
+with open('src/index.html', 'w') as f:
+    f.write('''<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="output.css" rel="stylesheet">
+  <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>''')
+
+os.system('start cmd /k npx live-server ./src')
+os.system("npx tailwindcss -i ./src/input.css -o ./src/output.css --watch")
