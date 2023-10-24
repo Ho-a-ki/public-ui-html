8cdc5477d04c4ee48aca53959d866b3aeafe2741
gh home desktop <gh4777@naver.com>
2023-10-24 22:54:22 +0900

    edit header flex

---
+++ tutorial/src/App.svelte
@@ -0,0 +1,21 @@
+
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Preview from "./lib/Preview.svelte";
+  import Header from "./lib/Header.svelte";
+</script>
+
+<main>
+  <Header></Header>
+  <Preview />
+</main>
+
+<style>
+  main {
+    scroll-behavior: smooth;
+    height: 5000px;
+  }
+</style>
+
---
+++ tutorial/src/lib/Header.svelte
@@ -0,0 +1,52 @@
+
+
+<nav>
+    <ul>
+        <li>하이</li>
+        <li>하이</li>
+        <li>하이</li>
+        <div>로고</div>
+        <li>하이</li>
+        <li>하이</li>
+        <li>하이</li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        height: 10vh;
+        border: 1px solid red;
+        width: min(100%, 1280px);
+        margin-inline: auto;
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 100%;
+    }
+    
+    
+    li {
+        width: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        flex: 1 1 auto;
+        line-height: 200%
+
+    }
+    
+    li:nth-child(2),
+    li:nth-child(3),
+    li:nth-child(6),
+    li:nth-child(7) {
+        border-left: 1px solid #ccc;
+        
+    }
+
+
+
+</style>
